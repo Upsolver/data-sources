4991f4e00e739dfd7b1fef802bb090b45d9757f1
Jason Fine <jasonf20@gmail.com>
2020-04-05 17:06:21 +0300

    ResultSetInputStream: Extracted row conversion code

---
+++ src/main/java/com/upsolver/datasources/jdbc/CsvRowConverter.java
@@ -0,0 +1,48 @@
+package com.upsolver.datasources.jdbc;
+
+import com.upsolver.datasources.jdbc.metadata.ColumnInfo;
+import com.upsolver.datasources.jdbc.metadata.TableInfo;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+class CsvRowConverter implements RowConverter {
+
+    private TableInfo tableInfo;
+
+    CsvRowConverter(TableInfo tableInfo) {
+        this.tableInfo = tableInfo;
+    }
+
+    @Override
+    public Boolean hasHeader() {
+        return true;
+    }
+
+
+    @Override
+    public void convertRow(String[] values, OutputStream os) throws IOException {
+        for (int i = 0; i < values.length; i++) {
+            os.write(values[i].getBytes());
+            writeCommaOrNewLine(os, i);
+        }
+    }
+
+    private void writeCommaOrNewLine(OutputStream byteArrayOutputStream, int i) throws IOException {
+        if (i < tableInfo.getColumnCount() - 1) {
+            byteArrayOutputStream.write(',');
+        } else {
+            byteArrayOutputStream.write('\n');
+        }
+    }
+
+    @Override
+    public void writeHeader(OutputStream os) throws IOException {
+        ColumnInfo[] columns = tableInfo.getColumns();
+        for (int i = 0; i < columns.length; i++) {
+            os.write(columns[i].getName().getBytes());
+            writeCommaOrNewLine(os, i);
+        }
+
+    }
+}
